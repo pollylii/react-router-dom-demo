17ef24d30593f0050df1f2ed53b7b18ada18962c
pollylilh <794211268@qq.com>
2021-05-24 17:40:17 +0800

    封装NavLink

--- README.md
+++ README.md
@@ -18,3 +18,37 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
     3.展示区写Route标签进行路径的匹配
         <Route path= " / xxXx' component={Demo}/>
     4.<App>的最外侧包裹了一个<BrowserRouter>或<HashRouter>
+
+### 路由组件与一般组件
+    1.写法不同:
+        一般组件: <Demo/ >
+        路由组件: <Route path=" / demo" component={Demo}/>
+    2.存放位置不同:
+        一般组件: components
+        路由组件: pages
+    3.接收到的props不同:
+        一般组件:写组件标签时传递了什么，就能收到什么
+        路由组任:接收到三个固定的属性
+            history:
+                go: f go(n)
+                goBack: f goBack()
+                goForward: f goForward()
+                push: f push(path,state)
+                replace: f replace(path,state)
+            location:
+                pathname : "/about"search: ""
+                state: undefined
+                match:
+                    params: {}
+                    path: " /about"
+                    url: "/about"
+
+### NavLink与封装NavLink
+    1.NavLink可以实现路由链接的高亮，通过activeclassName指定样式名
+    2.标签体内容是一个特殊的标签属性
+    3.通过this.props.children可以获取标签体内容 
+
+### Switch的使用
+    1.通常情况下. path和component是一一对应的关系。
+    2.Switch可以提高路由匹配效率(单一匹配)。
+
--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component} from 'react'
-import {Link,Route} from 'react-router-dom'
-
-import Home from './components/Home'
-import About from './components/About'
-
+import React, { Component } from 'react'
+import {Route,Switch} from 'react-router-dom'
+import Home from './pages/Home' //Home是路由组件
+import About from './pages/About' //About是路由组件
+import Header from './components/Header' //Header是一般组件
+import MyNavLink from './components/MyNavLink'
 
 
 export default class App extends Component {
@@ -12,23 +12,30 @@ export default class App extends Component {
 			<div>
 				<div className="row">
 					<div className="col-xs-offset-2 col-xs-8">
-						<div className="page-header"><h2>React Router Demo</h2></div>
+						<Header/>
 					</div>
 				</div>
 				<div className="row">
 					<div className="col-xs-2 col-xs-offset-2">
 						<div className="list-group">
+
+							{/* 原生html中，靠<a>跳转不同的页面 */}
+							{/* <a className="list-group-item" href="./about.html">About</a>
+							<a className="list-group-item active" href="./home.html">Home</a> */}
+
 							{/* 在React中靠路由链接实现切换组件--编写路由链接 */}
-              <Link className="list-group-item" to="/about">About</Link>
-              <Link className="list-group-item" to="/home">Home</Link>
+							<MyNavLink to="/about">About</MyNavLink>
+							<MyNavLink to="/home">Home</MyNavLink>
 						</div>
 					</div>
 					<div className="col-xs-6">
 						<div className="panel">
 							<div className="panel-body">
-                {/* 注册路由 */}
-                <Route path="/about" component={About}/>
-                <Route path="/home" component={Home}/>
+								{/* 注册路由 */}
+								<Switch>
+									<Route path="/about" component={About}/>
+									<Route path="/home" component={Home}/>
+								</Switch>
 							</div>
 						</div>
 					</div>
