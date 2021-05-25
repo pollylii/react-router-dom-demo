f177c0b304f04a3aa059161bc7283c1a7337ddef
pollylilh <794211268@qq.com>
2021-05-25 17:09:07 +0800

    编程式路由导航、BrowserRouter与HashRouter的区别

--- src/pages/Home/Message/index.jsx
+++ src/pages/Home/Message/index.jsx
@@ -10,6 +10,42 @@ export default class Message extends Component {
 			{id:'03',title:'消息3'},
 		]
 	}
+
+	replaceShow = (id,title)=>{
+		//replace跳转+携带params参数
+		//this.props.history.replace(`/home/message/detail/${id}/${title}`)
+
+		//replace跳转+携带search参数
+		// this.props.history.replace(`/home/message/detail?id=${id}&title=${title}`)
+
+		//replace跳转+携带state参数
+		this.props.history.replace(`/home/message/detail`,{id,title})
+	}
+
+	pushShow = (id,title)=>{
+		//push跳转+携带params参数
+		// this.props.history.push(`/home/message/detail/${id}/${title}`)
+
+		//push跳转+携带search参数
+		// this.props.history.push(`/home/message/detail?id=${id}&title=${title}`)
+
+		//push跳转+携带state参数
+		this.props.history.push(`/home/message/detail`,{id,title})
+		
+	}
+
+	back = ()=>{
+		this.props.history.goBack()
+	}
+
+	forward = ()=>{
+		this.props.history.goForward()
+	}
+
+	go = ()=>{
+		this.props.history.go(-2)
+	}
+
 	render() {
 		const {messageArr} = this.state
 		return (
@@ -29,6 +65,8 @@ export default class Message extends Component {
 									{/* 向路由组件传递state参数 */}
 									<Link to={{pathname:'/home/message/detail',state:{id:msgObj.id,title:msgObj.title}}}>{msgObj.title}</Link>
 
+									&nbsp;<button onClick={()=> this.pushShow(msgObj.id,msgObj.title)}>push查看</button>
+									&nbsp;<button onClick={()=> this.replaceShow(msgObj.id,msgObj.title)}>replace查看</button>
 								</li>
 							)
 						})
@@ -43,6 +81,10 @@ export default class Message extends Component {
 
 				{/* state参数无需声明接收，正常注册路由即可 */}
 				<Route path="/home/message/detail" component={Detail}/>
+
+				<button onClick={this.back}>回退</button>&nbsp;
+				<button onClick={this.forward}>前进</button>&nbsp;
+				<button onClick={this.go}>go</button>
 
 			</div>
 		)
